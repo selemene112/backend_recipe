29fbca8ba29e3c3d3378f1cbd34a7512d58bed19
selemene112 <ariyanda98y@gmail.com>
2023-07-17 08:24:32 +0800

    JWT Vertifikasi end Generate Token

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -1,8 +1,11 @@
 const jwt = require('jsonwebtoken');
 const secretKey = 'secretKey123';
 
-const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization');
+const VertifikasiToken = (req, res, next) => {
+  //   console.log(req.header);
+  //   console.log(token);
+  const generateToken = req.header('Authorization');
+  const token = generateToken.split(' ')[1];
 
   if (!token) {
     return res.status(401).json({
@@ -14,7 +17,7 @@ const authMiddleware = (req, res, next) => {
   try {
     // Verifikasi token
     const decoded = jwt.verify(token, secretKey);
-    req.userId = decoded.userId;
+    req.id = decoded.id;
     next();
   } catch (error) {
     return res.status(401).json({
@@ -24,4 +27,28 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
-module.exports = authMiddleware;
+const generateToken = (req, res) => {
+  const { id } = req;
+
+  try {
+    // Buat token dengan payload berisi ID pengguna
+    const token = jwt.sign({ id }, secretKey, { expiresIn: '1h' });
+
+    res.status(200).json({
+      success: true,
+      message: 'Token generated successfully',
+      token: token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Error generating token',
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  VertifikasiToken,
+  generateToken,
+};
--- src/route/user.js
+++ src/route/user.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const route = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
+// const authMiddleware = require('../middleware/authMiddleware');
+const jwt = require('../middleware/jwt');
 const hashPasswordMiddleware = require('../middleware/bcryptjs');
 const UserController = require('../controller/ControllerUser.js');
 
-route.get('/', authMiddleware, UserController.GetAllUser);
+route.get('/', jwt.VertifikasiToken, UserController.GetAllUser);
 route.get('/:id', UserController.GetAllUserById);
-route.post('/', hashPasswordMiddleware, UserController.CreateDataUser);
+route.post('/', hashPasswordMiddleware, jwt.generateToken, UserController.CreateDataUser);
 route.put('/:id', UserController.UpdateUser);
 route.delete('/:id', UserController.DeleteUser);
 module.exports = route;
