47555072b9a955b7ec957755a0d32ca8eec80a1a
selemene112 <ariyanda98y@gmail.com>
2023-07-22 05:19:05 +0800

    add env secretkey

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,3 @@
-// const { Client } = require('pg');
-// const client = new Client({
-//   user: 'admin',
-//   host: 'localhost',
-//   database: 'pijarcamp',
-//   password: 'lele123',
-//   port: 5432,
-// });
-// client.connect(function (err) {
-//   if (err) throw err;
-//   console.log('Connected!');
-// });
-
 const Pool = require('pg').Pool;
 const pool = new Pool({
   user: process.env.user,
